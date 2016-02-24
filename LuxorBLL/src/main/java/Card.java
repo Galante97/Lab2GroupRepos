ae91db067503617aa1d90fb6beae24f60ee16709
YOUR NAME <esteband@udel.edu>
2016-02-24 13:00:58 -0500

    Update 1.1

--- LuxorBLL/src/main/java/Card.java
+++ LuxorBLL/src/main/java/Card.java
@@ -1,4 +1,18 @@
 
-public class Card {
+public class Card{
+	private Suit suit;
+	private Value value;
+	
+	public Card (Value val, Suit suitVal){
+		suit=suitVal;
+		value=val;
+	}
 
+	private Value getValue() {
+		return value;
+	}
+
+	private Suit getSuit() {
+		return suit;
+	}
 }
